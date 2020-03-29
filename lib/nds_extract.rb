e41c2bfbf53752f20cf5c42d3dad22ad215623aa
Richard Chea <rchea817@gmail.com>
2020-03-29 22:54:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,11 +2,10 @@ def directors_totals(nds)
  total = {}
  i = 0
  while i < nds.length do
-    d_name = nds[i][:name]
     j = 0
-    total [d_name] = 0
+    total [nds[i][:name]] = 0
     while j < nds[i][:movies].length do
-      total [d_name] += nds[i][:movies][j][:worldwide_gross]
+      total [nds[i][:name]] += nds[i][:movies][j][:worldwide_gross]
       j += 1
     end
   i += 1
